d1811618ab57c9c27a8d84c033192aed0d7e64da
watislaf <vladkozulin@mail.ru>
2022-01-31 05:36:23 +0300

    * setup sh fixed

--- setupDocker.sh
+++ setupDocker.sh
@@ -14,6 +14,6 @@
 #sudo usermod -aG docker $USER
 # newgrp docker
 docker pull gradle
-
+docker stop $( docker container ls | cut -d ' '  -f  1 | tail -1 )
 docker build ./ -t elevators
-docker run -b -p 6778:6778 elevators  gradle run &
+docker run -d -p 6778:6778 elevators  gradle run &
