4c5b3c6ac53cd9702d63876ff84c0e3690f3e2e7
watislaf <vladkozulin@mail.ru>
2022-01-31 03:05:42 +0300

    * port fix

--- app/src/main/java/connector/clientServer/ConnectionSettings.java
+++ app/src/main/java/connector/clientServer/ConnectionSettings.java
@@ -4,5 +4,5 @@ public class ConnectionSettings {
     static public final double SSPS = 10;       // server send per second
 
     static final String HOST = "localhost";
-    static final int PORT = 1988;
+    static final int PORT = 6778;
 }
